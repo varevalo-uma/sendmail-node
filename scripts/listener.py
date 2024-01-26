6cca86038a624ff7fc09d10f4f84d295df908af6
varevalo-uma <varevalo@garthim.uma.es>
2024-01-26 23:52:19 +0100

    New version of the node. Now I use smtplib for sending messages instead of postfix & mailutils.

--- scripts/listener.py
+++ scripts/listener.py
@@ -1,37 +1,73 @@
 #!/usr/bin/env python
 
-## Simple listener that send mails using postfix & mailutils & google.
-## You can send emails easyly from command line as indicated next:
-##    eg. rostopic pub /chatter std_msgs/String '{data: "{ \"to\": \"garthim.uma@gmail.com\", \"subject\":\"test\", \"body\":\"hey\"}"}'
+## Send emails by means of a Google Account.
 
 import rospy
 from std_msgs.msg import String
+
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import formataddr
 import json
-import subprocess
+import smtplib, ssl
+
+
+# Google credentials
+from_addr = "your@email.com"
+password = "yourpassword"
+
+
+# SMTP Server
+server = "smtp.gmail.com" # domain or ip
+port = 587  # for starttls
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + " %s", data.data)
+
+    rospy.loginfo(rospy.get_caller_id() + " " + data.data)
+
+    # parse the mail message
     email = json.loads(data.data)
-    #print(email["to"])
-    #print(email["subject"])
-    #print(email["body"])
-    cmd = 'echo "' + email["body"] + '" | mail -s "' + email["subject"] + '" ' + email["to"]
-    #print(cmd)
-    subprocess.run(cmd, shell=True)
+
+    to_addr = email["to"]
+
+    # Prepare the message
+    message = MIMEText(email["body"])
+    message['Subject'] = email["subject"]
+    message['From'] = formataddr( (email["robot"], from_addr) )
+    message['To'] = to_addr
+
+    # Try to log in to server and send the email
+    try:
+
+        # Create a secure SSL context
+        ctx = ssl.create_default_context()
+
+        # Create a SMTP client
+        smtp = smtplib.SMTP(server, port)
+
+        smtp.starttls(context = ctx) # secure the connection
+
+        smtp.login(from_addr, password)
+
+        #print("\nmessage: \n", message.as_string())
+        smtp.send_message(message)
+
+    except Exception as e:
+
+        # Print any error messages to stdout
+        print(e)
+
+    finally:
+        smtp.quit()
 
 
 def listener():
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+    rospy.init_node('listener')
 
-    rospy.Subscriber("chatter", String, callback)
+    rospy.Subscriber("chatter", String, callback) # listening at /chatter
 
-    # spin() simply keeps python from exiting until this node is stopped
+    # waiting for new messages
     rospy.spin()
 
 if __name__ == '__main__':
--- scripts/talker.py
+++ scripts/talker.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python
 
-## Simple talker that send mails by means of a relay that listens the "chatter" topic.
-## Be carefull, the struct of json objecti must be as indicated next:
-##    eg: '{ "to" : "garthim.uma@gmail.com", "subject" : "test", "body" : "This is a test. bye. V" }'
+## This node sends mails by means of sendmail, which is listening the "chatter" topic.
+## Be careful, the struct of json object must be as indicated next:
+##    eg: email = '{ 
+##          "robot" : "name",
+##          "to" : "to@example.com", 
+##          "subject" : "Hi!",
+##          "body" : "This is a test. bye. V"
+##      }'
 
 import rospy
 from std_msgs.msg import String
 
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+
+    pub = rospy.Publisher('chatter', String)
     rospy.init_node('talker', anonymous=True)
-    mail = '{ "to" : "garthim.uma@gmail.com", "subject" : "test", "body" : "This is a test. bye. V" }'
-    rospy.loginfo(mail)
-    pub.publish(mail)
+
+    email = "{ \
+        'robot' : 'name', \
+        'to' : 'to@example.com', \
+        'subject' : 'Hi!', \
+        'body' : 'This is a test. bye. V' \
+    }"
+    rospy.loginfo(email)
+
+    pub.publish(email)
 
 
 if __name__ == '__main__':
